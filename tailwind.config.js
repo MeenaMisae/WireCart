6f45956503fb24031823c5ab6158b6c3fbf3815f
MeenaMisae <meenahiwatashi@gmail.com>
2024-02-07 07:41:18 -0300

    start: 🎉

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+import daisyui from 'daisyui'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+    './storage/framework/views/*.php',
+    './resources/views/**/*.blade.php',
+    './app/Livewire/**/*.php',
+    './resources/js/**/*.js'
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+}
+
