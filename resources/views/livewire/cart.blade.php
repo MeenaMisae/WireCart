f39451494b2f069848f647ce7d6c98cd43f69f21
MeenaMisae <meenahiwatashi@gmail.com>
2024-02-11 19:27:39 -0300

    wip: 🚧 Criação do componente de carrinho em livewire (teste)

---
+++ resources/views/livewire/cart.blade.php
@@ -0,0 +1,223 @@
+@section('title', 'Carrinho')
+
+<div>
+    <div class="w-screen h-screen flex justify-center items-center bg-[#F9F9F9]">
+        <div class="flex">
+            <div class="h-[35rem] w-[50rem] mt-28">
+                <div class="grid grid-cols-1 grid-rows-5 w-full h-full gap-5">
+                    <div class="grid grid-cols-5 bg-[#EBEBEB]">
+                        <div class="flex justify-center items-center">
+                            <div class="w-[4.5rem] h-[4.5rem] avatar">
+                                <div class="rounded-full">
+                                    <img src="{{ asset('images/dark-green-t-shirt.jpg') }}" />:
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex flex-col justify-center items-center">
+                            <span class="font-semibold text-sm">Denim T-Shirt</span>
+                            <span class="text-sm">Ref. 007197456</span>
+                        </div>
+                        <div class="flex justify-center items-center">
+                            <span class="font-semibold text-sm">Café</span>
+                        </div>
+                        <div class="flex items-center justify-center">
+                            <div>
+                                <span class="mr-1.5">2</span>
+                            </div>
+                            <div>
+                                <button>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="#959595" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="#EBEBEB" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                    </svg>
+                                </button>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="#959595" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="#EBEBEB" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                </svg>
+                            </div>
+                        </div>
+                        <div class="flex justify-center items-center relative">
+                            <span class="font-semibold ml-9">R$ 80.00</span>
+                            <div class="absolute left-52">
+                                <button>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="#959595" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="grid grid-cols-5 bg-[#EBEBEB]">
+                        <div class="flex justify-center items-center">
+                            <div class="w-[4.5rem] h-[4.5rem] avatar">
+                                <div class="rounded-full">
+                                    <img src="{{ asset('images/dark-green-t-shirt.jpg') }}" />:
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex flex-col justify-center items-center">
+                            <span class="font-semibold text-sm">Denim T-Shirt</span>
+                            <span class="text-sm">Ref. 007197456</span>
+                        </div>
+                        <div class="flex justify-center items-center">
+                            <span class="font-semibold text-sm">Café</span>
+                        </div>
+                        <div class="flex items-center justify-center">
+                            <div>
+                                <span class="mr-1.5">2</span>
+                            </div>
+                            <div>
+                                <button>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="#959595" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="#EBEBEB" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                    </svg>
+                                </button>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="#959595" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="#EBEBEB" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                </svg>
+                            </div>
+                        </div>
+                        <div class="flex justify-center items-center relative">
+                            <span class="font-semibold ml-9">R$ 80.00</span>
+                            <div class="absolute left-52">
+                                <button>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="#959595" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="grid grid-cols-5 bg-[#EBEBEB]">
+                        <div class="flex justify-center items-center">
+                            <div class="w-[4.5rem] h-[4.5rem] avatar">
+                                <div class="rounded-full">
+                                    <img src="{{ asset('images/dark-green-t-shirt.jpg') }}" />:
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex flex-col justify-center items-center">
+                            <span class="font-semibold text-sm">Denim T-Shirt</span>
+                            <span class="text-sm">Ref. 007197456</span>
+                        </div>
+                        <div class="flex justify-center items-center">
+                            <span class="font-semibold text-sm">Café</span>
+                        </div>
+                        <div class="flex items-center justify-center">
+                            <div>
+                                <span class="mr-1.5">2</span>
+                            </div>
+                            <div>
+                                <button>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="#959595" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="#EBEBEB" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                    </svg>
+                                </button>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="#959595" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="#EBEBEB" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                </svg>
+                            </div>
+                        </div>
+                        <div class="flex justify-center items-center relative">
+                            <span class="font-semibold ml-9">R$ 80.00</span>
+                            <div class="absolute left-52">
+                                <button>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="#959595" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="grid grid-cols-5 bg-[#EBEBEB]">
+                        <div class="flex justify-center items-center">
+                            <div class="w-[4.5rem] h-[4.5rem] avatar">
+                                <div class="rounded-full">
+                                    <img src="{{ asset('images/denim-pants.webp') }}" />:
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex flex-col justify-center items-center">
+                            <span class="font-semibold text-sm">Calça Denim</span>
+                            <span class="text-sm">Ref. 007197456</span>
+                        </div>
+                        <div class="flex justify-center items-center">
+                            <span class="font-semibold text-sm">Grisáceo</span>
+                        </div>
+                        <div class="flex items-center justify-center">
+                            <div>
+                                <span class="mr-1.5">2</span>
+                            </div>
+                            <div>
+                                <button>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="#959595" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="#EBEBEB" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                    </svg>
+                                </button>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="#959595" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="#EBEBEB" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                </svg>
+                            </div>
+                        </div>
+                        <div class="flex justify-center items-center relative">
+                            <span class="font-semibold ml-9">R$ 80.00</span>
+                            <div class="absolute left-52">
+                                <button>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="#959595" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="bg-slate items-center flex justify-between">
+                        <div class="flex items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18" />
+                            </svg>
+                            <span class="font-bold ml-4 text-sm">Voltar para Loja</span>
+                        </div>
+                        <div class="flex space-x-16 mx-6">
+                            <div>
+                                <span>Subtotal:</span>
+                            </div>
+                            <div>
+                                <span class="font-bold">R$ 400.00</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {{-- <div class="ml-8 mt-36">
+                <button>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="#959595" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                    </svg>
+                </button>
+            </div> --}}
+
+        </div>
+    </div>
+</div>
