1afa439db265f2206ef7ead046e9aa355b549462
MeenaMisae <meenahiwatashi@gmail.com>
2024-02-11 19:25:26 -0300

    style: 💄 Criação do componente da navbar

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,71 @@
+<div class="navbar bg-base-100 p-4 h-24">
+    <div class="navbar-start">
+        <h1 class="text-3xl font-light tracking-[0.3rem]">wire:cotton</h1>
+    </div>
+    <div class="navbar-center gap-6">
+        <div class="dropdown dropdown-hover h-12 mt-6">
+            <a tabindex="0" class="text-xl tracking-[0.2rem] hover:font-semibold cursor-pointer">feminino</a>
+            <ul tabindex="0" class="dropdown-content z-[1] p-3 shadow bg-base-100 rounded-box w-28 mt-4">
+                <li class="flex flex-col">
+                    <a href="" class="btn btn-sm btn-ghost">new</a>
+                    <a href="" class="btn btn-sm btn-ghost">t-shirts</a>
+                    <a href="" class="btn btn-sm btn-ghost">blusas</a>
+                    <a href="" class="btn btn-sm btn-ghost">vestidos</a>
+                    <a href="" class="btn btn-sm btn-ghost">calças</a>
+                </li>
+            </ul>
+        </div>
+        <div class="dropdown dropdown-hover h-12 mt-6">
+            <a tabindex="1" class="text-xl tracking-[0.2rem] hover:font-semibold cursor-pointer">masculino</a>
+            <ul tabindex="1" class="dropdown-content z-[1] p-3 shadow bg-base-100 rounded-box w-28 mt-4">
+                <li class="flex flex-col">
+                    <a href="" class="btn btn-sm btn-ghost">new</a>
+                    <a href="" class="btn btn-sm btn-ghost">t-shirts</a>
+                    <a href="" class="btn btn-sm btn-ghost">blusas</a>
+                    <a href="" class="btn btn-sm btn-ghost">calças</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="navbar-end">
+        <div class="mr-3 flex justify-center items-center">
+            <div class="dropdown dropdown-hover h-12 mt-6 justify-center flex">
+                <a tabindex="0" class="text-xl tracking-tighter mr-2 hover:font-semibold cursor-pointer">SALE</a>
+                <ul tabindex="0" class="dropdown-content z-[1] p-3 shadow bg-base-100 rounded-box w-28 mt-9">
+                    <li class="flex flex-col">
+                        <a href="" class="btn btn-sm btn-ghost">feminino</a>
+                        <a href="" class="btn btn-sm btn-ghost">masculino</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <button class="btn btn-ghost btn-circle">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+            </svg>
+        </button>
+        <div class="dropdown dropdown-end">
+            <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+                <div class="indicator">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                    </svg>
+                    <span class="badge badge-sm indicator-item">8</span>
+                </div>
+            </div>
+            <div tabindex="0" class="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
+                <div class="card-body">
+                    <span class="font-bold text-lg">8 Items</span>
+                    <span class="text-info">Subtotal: $999</span>
+                    <div class="card-actions">
+                        <button class="btn btn-primary btn-block">View cart</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
