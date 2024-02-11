31a9c0739d987c482c42f8813555b2a92244a43b
MeenaMisae <meenahiwatashi@gmail.com>
2024-02-11 19:21:03 -0300

    chore: 🗃️ Criação do model de Produtos

---
+++ app/Models/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Product extends Model
+{
+    use HasFactory;
+    protected $fillable = ['category_id', 'subcategory_id', 'price', 'name', 'description', 'slug', 'on_sale', 'discount'];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+
+    public function subcategory(): BelongsTo
+    {
+        return $this->belongsTo(Subcategory::class, 'subcategory_id');
+    }
+
+}
