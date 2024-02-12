f08affebafddb441e0f968e03a53b6f955c25251
MeenaMisae <meenahiwatashi@gmail.com>
2024-02-12 13:18:43 -0300

    style: 💄 Isolando menu lateral do admin num componente

---
+++ resources/views/components/admin/side-menu.blade.php
@@ -0,0 +1,15 @@
+<li>
+    <a href="{{ route('admin.products.index') }}" class="text-lg tracking-[0.2rem] hover:font-semibold cursor-pointer">produtos</a>
+</li>
+<li>
+    <a class="text-lg tracking-[0.2rem] hover:font-semibold cursor-pointer">categorias</a>
+</li>
+<li>
+    <a class="text-lg tracking-[0.2rem] hover:font-semibold cursor-pointer">subcategorias</a>
+</li>
+<li>
+    <a class="text-lg tracking-[0.2rem] hover:font-semibold cursor-pointer">pedidos</a>
+</li>
+<li>
+    <a class="text-lg tracking-[0.2rem] hover:font-semibold cursor-pointer">clientes</a>
+</li>
