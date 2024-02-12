1aa1f1e6a0d68c695104f838b2b7663e227481ab
MeenaMisae <meenahiwatashi@gmail.com>
2024-02-12 13:18:20 -0300

    style: 💄 Isolando menu hamburghuer num componente

---
+++ resources/views/components/admin/hamburguer-menu.blade.php
@@ -0,0 +1,12 @@
+<div class="flex-none lg:hidden">
+    <label for="my-drawer-3" aria-label="open sidebar" class="btn btn-square btn-ghost">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+            stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12" />
+        </svg>
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+            d="M4 6h16M4 12h16M4 18h16"></path>
+        </svg>
+    </label>
+</div>
