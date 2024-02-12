a19d64422b61be55376a26535db28871949b1234
MeenaMisae <meenahiwatashi@gmail.com>
2024-02-12 08:28:52 -0300

    style: 📱 Navbar responsiva

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -12,10 +12,33 @@
 </head>
 
 <body>
-    <div class="h-screen w-screen">
-        @include('components.admin.navbar')
-        @yield('content')
-    </div>
+    <div class="drawer">
+        <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
+        <div class="drawer-content flex flex-col">
+          <div class="w-full navbar">
+            <div class="flex-none lg:hidden">
+              <label for="my-drawer-3" aria-label="open sidebar" class="btn btn-square btn-ghost">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+              </label>
+            </div>
+            <div class="flex-none hidden lg:block">
+              <ul class="menu menu-horizontal w-[99vw]">
+                @include('components.admin.navbar')
+
+              </ul>
+            </div>
+          </div>
+          @yield('content')
+        </div>
+        <div class="drawer-side">
+          <label for="my-drawer-3" aria-label="close sidebar" class="drawer-overlay"></label>
+          <ul class="menu p-4 w-80 min-h-full bg-base-200">
+            <li><a>Sidebar Item 1</a></li>
+            <li><a>Sidebar Item 2</a></li>
+          </ul>
+        </div>
+      </div>
+
     @yield('js')
 </body>
 
