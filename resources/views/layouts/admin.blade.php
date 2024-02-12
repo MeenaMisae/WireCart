2f865e7ca928969263d5aa7be1ce70f1e12561b3
MeenaMisae <meenahiwatashi@gmail.com>
2024-02-12 13:19:06 -0300

    style: 💄 Incluindo componente do menu hamburguer

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -16,15 +16,7 @@
         <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
         <div class="drawer-content flex flex-col">
             <div class="w-full navbar">
-                <div class="flex-none lg:hidden">
-                    <label for="my-drawer-3" aria-label="open sidebar" class="btn btn-square btn-ghost">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                            class="inline-block w-6 h-6 stroke-current">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M4 6h16M4 12h16M4 18h16"></path>
-                        </svg>
-                    </label>
-                </div>
+                @include('components.admin.hamburguer-menu')
                 <div class="px-2 mx-2 lg:hidden w-full flex justify-center">
                     <a href="{{ route('admin.index') }}">
                         <h1 class="text-3xl font-light tracking-[0.3rem]">wire:cotton</h1>
@@ -38,26 +30,7 @@
             </div>
             @yield('content')
         </div>
-        <div class="drawer-side">
-            <label for="my-drawer-3" aria-label="close sidebar" class="drawer-overlay"></label>
-            <ul class="menu p-4 w-80 min-h-full bg-base-200">
-                <li>
-                    <a class="text-lg tracking-[0.2rem] hover:font-semibold cursor-pointer">produtos</a>
-                </li>
-                <li>
-                    <a class="text-lg tracking-[0.2rem] hover:font-semibold cursor-pointer">categorias</a>
-                </li>
-                <li>
-                    <a class="text-lg tracking-[0.2rem] hover:font-semibold cursor-pointer">subcategorias</a>
-                </li>
-                <li>
-                    <a class="text-lg tracking-[0.2rem] hover:font-semibold cursor-pointer">pedidos</a>
-                </li>
-                <li>
-                    <a class="text-lg tracking-[0.2rem] hover:font-semibold cursor-pointer">clientes</a>
-                </li>
-            </ul>
-        </div>
+        @include('components.admin.drawer-side')
     </div>
 
     @yield('js')
