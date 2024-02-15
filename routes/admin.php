3846b0308f9f178883d0699834f5aa91595db176
MeenaMisae <meenahiwatashi@gmail.com>
2024-02-15 09:45:25 -0300

    feat: 🚧 incluindo rotas de categoria e subcategoria no arquivo principal de rotas admin

--- routes/admin.php
+++ routes/admin.php
@@ -6,4 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('admin')->group(static function () {
     Route::get('', [AdminController::class, 'index'])->name('admin.index');
     require __DIR__ . '/admin/products.php';
+    require __DIR__ . '/admin/categories.php';
+    require __DIR__ . '/admin/subcategories.php';
 });
