b004a5817f47c622ccb4c11778f43d37542ff885
MeenaMisae <meenahiwatashi@gmail.com>
2024-02-15 09:38:27 -0300

    chore: 🦺 Adicionando validação na criação de produto e carregamento dinamico de subcategorias

--- app/Livewire/Admin/AdminCreateProduct.php
+++ app/Livewire/Admin/AdminCreateProduct.php
@@ -2,7 +2,11 @@
 
 namespace App\Livewire\Admin;
 
+use App\Models\Category;
+use App\Models\Subcategory;
+use Illuminate\Support\Collection;
 use Illuminate\View\View;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class AdminCreateProduct extends Component
@@ -11,6 +15,25 @@ class AdminCreateProduct extends Component
     public bool $onSale = false;
     public $productFinalPrice;
     public $productPrice;
+    #[Validate('required|string|min:3', as: 'produto')]
+    public $productName;
+    #[Validate('required|exists:categories,id', as: 'categoria')]
+    public $categoryID = 0;
+    #[Validate('required|exists:subcategories,id', as: 'subcategoria')]
+    public $subcategoryID = 0;
+    public Collection $categories;
+    public $subcategories;
+
+    public function messages(): array
+    {
+        return [
+            'subcategoryID.exists' => 'subcategoria inválida',
+        ];
+    }
+    public function mount(): void
+    {
+        $this->categories = Category::all();
+    }
 
     public function calculateDiscount(): void
     {
@@ -20,8 +43,20 @@ class AdminCreateProduct extends Component
         $this->productFinalPrice = $this->productPrice - $this->productPrice * ($this->productDiscount / 100);
     }
 
+    public function loadSubcategories(): void
+    {
+        $this->subcategories = Subcategory::where('category_id', '=', $this->categoryID)->get();
+    }
+
+    public function createProduct(): void
+    {
+        $this->validate();
+    }
+
     public function render(): View
     {
-        return view('livewire.admin.admin-create-product');
+        return view('livewire.admin.admin-create-product', [
+            'categories' => $this->categories,
+        ]);
     }
 }
