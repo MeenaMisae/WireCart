8f10dcdba874fd01dec30a714ae9f5918cc91271
MeenaMisae <meenahiwatashi@gmail.com>
2024-02-23 08:38:17 -0300

    feat: 🚧 escondendo navbar para a criação de produto e adicionando tds os produtos no mount

--- app/Livewire/Admin/Products/AdminProductIndex.php
+++ app/Livewire/Admin/Products/AdminProductIndex.php
@@ -2,6 +2,9 @@
 
 namespace App\Livewire\Admin\Products;
 
+use App\Livewire\Admin\Components\Navbar;
+use App\Models\Product;
+use Illuminate\Support\Collection;
 use Illuminate\View\View;
 use Livewire\Component;
 
@@ -9,21 +12,32 @@ class AdminProductIndex extends Component
 {
     public bool $showProducts = true;
     public bool $showAddProductForm = false;
+    public $productTable;
+    public Collection $products;
+
+    public function mount(): void
+    {
+        $this->products = Product::all();
+    }
 
     public function showProductsTable(): void
     {
         $this->showProducts = true;
         $this->showAddProductForm = false;
+        $this->dispatch('show_navbar')->to(Navbar::class);
     }
 
     public function addProduct(): void
     {
         $this->showAddProductForm = true;
         $this->showProducts = false;
+        $this->dispatch('hide_navbar')->to(Navbar::class);
     }
 
     public function render(): View
     {
-        return view('livewire.admin.products.admin-product-index');
+        return view('livewire.admin.products.admin-product-index', [
+            'products' => $this->products,
+        ]);
     }
 }
