bb2188009aa2dc21c4e64119e3cd1fcb2bcc2373
MeenaMisae <meenahiwatashi@gmail.com>
2024-02-27 09:07:45 -0300

    wip: 🚧 isolando formulario de revisao de produto em um componente

---
+++ app/Livewire/Admin/Products/Create/Form/AdminCreateProductReviewForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin\Products\Create\Form;
+
+use Livewire\Component;
+
+class AdminCreateProductReviewForm extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.products.create.form.admin-create-product-review-form');
+    }
+}
---
+++ resources/views/livewire/admin/products/create/form/admin-create-product-review-form.blade.php
@@ -0,0 +1,10 @@
+<div class="bg-base-200 rounded-lg shadow h-full w-3/4 py-4 mt-7" x-show="currentStep === 3" x-transition.500ms>
+    REVIEW
+    <button class="btn btn-outline btn-sm" x-on:click="currentStep--">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+            class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18" />
+        </svg>
+
+    </button>
+</div>
