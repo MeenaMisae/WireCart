e33f2a94bf0cfb731a75e7c1fa528c1e454f857f
MeenaMisae <meenahiwatashi@gmail.com>
2024-02-29 07:26:55 -0300

    wip: 🚧 adicionando método que remove imagem do array

--- app/Livewire/Admin/Products/Create/Form/AdminCreateProductImageForm.php
+++ app/Livewire/Admin/Products/Create/Form/AdminCreateProductImageForm.php
@@ -10,6 +10,12 @@ class AdminCreateProductImageForm extends Component
     use WithFileUploads;
     public $files = [];
 
+    public function removeImage($imageID): void
+    {
+        unset($this->files[$imageID]);
+        $this->files = array_values($this->files);
+    }
+
     public function render()
     {
         return view('livewire.admin.products.create.form.admin-create-product-image-form');
