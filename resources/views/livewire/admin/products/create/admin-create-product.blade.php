2344c4261be440ee8115ddfda02d85c6000b9498
MeenaMisae <meenahiwatashi@gmail.com>
2024-03-04 09:38:41 -0300

    feat: 💄 adicionando um icone de checked conforme o usuario vai passando os steps

--- resources/views/livewire/admin/products/create/admin-create-product.blade.php
+++ resources/views/livewire/admin/products/create/admin-create-product.blade.php
@@ -1,15 +1,18 @@
 <div>
-    <div class="lg:flex justify-center items-center hidden lg:p-5">
+    <div class="items-center justify-center hidden lg:flex lg:p-5">
         <h1 class="text-3xl font-light tracking-[0.2rem]">adicionar novo produto</h1>
     </div>
-    <div class="flex flex-col justify-center items-center" x-data="{ currentStep: 2 }">
+        <div class="flex flex-col items-center justify-center" x-data="{ currentStep: 2 }">
         <ul class="steps steps-horizontal">
-            <li class="step step-info">Formulário</li>
-            <li class="step" :class="currentStep > 1 && currentStep <= 3 ?  'step-info' : '' ">Fotos</li>
-            <li class="step" :class="currentStep >= 2 && currentStep === 3  ? 'step-info' : '' ">Revisão</li>
+            <li :data-content="currentStep > 1 ? '✓' : '1'" class="step step-info">Formulário</li>
+            <li :data-content="currentStep > 2 && currentStep <= 3 ? '✓' : '2'" id="imageForm" class="step"
+                :class="currentStep > 1 && currentStep <= 3 ? 'step-info' : ''">Fotos</li>
+            <li  class="step" :class="currentStep >= 2 && currentStep === 3 ? 'step-info' : ''">
+                Revisão
+            </li>
         </ul>
-            <livewire:admin.products.create.form.admin-create-product-form />
-            <livewire:admin.products.create.form.admin-create-product-image-form />
-            <livewire:admin.products.create.form.admin-create-product-review-form />
+        <livewire:admin.products.create.form.admin-create-product-form />
+        <livewire:admin.products.create.form.admin-create-product-image-form />
+        <livewire:admin.products.create.form.admin-create-product-review-form />
     </div>
 </div>
