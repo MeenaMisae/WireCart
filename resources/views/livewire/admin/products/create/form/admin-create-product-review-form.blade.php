900ce6ec2a871cd0c65bbbe72184c1b1b45de938
MeenaMisae <meenahiwatashi@gmail.com>
2024-03-17 12:35:01 -0300

    wip: 🚧 construção da pagina de review das informações do produto

--- resources/views/livewire/admin/products/create/form/admin-create-product-review-form.blade.php
+++ resources/views/livewire/admin/products/create/form/admin-create-product-review-form.blade.php
@@ -1,59 +1,80 @@
 <div class="w-3/4 h-full py-4 rounded-lg shadow bg-base-200 mt-7" x-show="currentStep === 3" x-transition.500ms>
-    <div class="grid h-full grid-cols-8 grid-rows-8">
-        <div class="flex items-center justify-center col-start-1 row-start-8">
-            <button class="btn btn-outline btn-sm" x-on:click="currentStep--">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18" />
-                </svg>
-            </button>
-        </div>
-        <div class="col-span-2 col-start-3 row-span-4 mt-3 mr-3 max-h-60">
-            <img src="https://placehold.co/1000x1500" alt="">
-        </div>
-        <div class="col-span-2 col-start-5 mt-3">
-            <h2 class="text-2xl tracking-[0.1rem] font-light w-full mt-2">{{ $productName }}</h2>
-            {{-- <span class="mr-1 badge badge-accent">{{ $category }}</span>
-            <span class="badge badge-accent">{{ $subcategory }}</span> --}}
-        </div>
-        <div class="col-start-5 max-h-12">
-            <h3 class="text-2xl tracking-[0.1rem] font-light">
-                @if ($onSale)
-                    <div class="mb-1">
-                        <div class="flex items-center gap-2">
-                            <div class="line-through text-sm">
-                                {{ 'R$' . $productPrice }}
-                            </div>
-                            <span
-                                class="badge badge-info badge-sm label-text font-semibold">{{ $productDiscount * 100 }}%</span>
+    <div class="flex justify-between w-full items-center p-8">
+        <button class="btn btn-outline btn-sm" x-on:click="currentStep--">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.0"
+                stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18" />
+            </svg>
+        </button>
+        <div class="flex gap-4">
+            <div class="w-[20rem]">
+                @isset($productImages)
+                    <div class="swiper mySwiper rounded-md shadow-sm">
+                        <div class="swiper-wrapper h-auto max-w-full object-cover">
+                            @foreach ($productImages as $image)
+                                <img class="swiper-slide cursor-pointer" src="{{ $image }}"
+                                    x-on:click="$('.swiper-button-next').click()">
+                            @endforeach
                         </div>
-                        <div>
-                            {{ 'R$' . $productFinalPrice }}
-                        </div>
+                        <div class="swiper-button-next hidden"></div>
                     </div>
-                @else
-                    {{ 'R$' . $productPrice }}
-                @endif
-            </h3>
+                @endisset
+            </div>
+            <div class="flex flex-col">
+                <div class="mb-2">
+                    <h2 class="text-2xl tracking-[0.1rem] font-light w-full mt-3">{{ $productName }}</h2>
+                </div>
+                <div>
+                    <div class="text-2xl tracking-[0.1rem] font-light">
+                        @if ($onSale)
+                            <div class="mb-1">
+                                <div class="flex items-center gap-2">
+                                    <div class="line-through text-sm">
+                                        {{ 'R$' . $productPrice }}
+                                    </div>
+                                    <span
+                                        class="badge badge-info badge-sm label-text font-semibold">{{ $productDiscount * 100 }}%</span>
+                                </div>
+                                <div>
+                                    {{ 'R$' . $productFinalPrice }}
+                                </div>
+                            </div>
+                        @else
+                            {{ 'R$' . $productPrice }}
+                        @endif
+                    </div>
+                </div>
+                <div class="h-auto w-80 mt-2">
+                    <p>{{ $productDescription }}</p>
+                </div>
+                <div class="mt-3">
+                    @isset($productQuantity)
+                        <span class="badge badge-warning font-semibold">{{ $productQuantity }} {{ $productQuantity > 1 ? 'disponíveis' : 'disponível'}}</span>
+                    @endisset
+                </div>
+            </div>
         </div>
-        <div class="col-span-2 col-start-5 row-start-3 max-h-12">
-            <p>{{ $productDescription }}</p>
-        </div>
-        <div class="flex items-center justify-center col-start-8 row-start-8">
-            <button class="btn btn-outline btn-sm">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.0"
-                    stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
-                </svg>
-            </button>
-        </div>
+        <button class="btn btn-outline btn-sm">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.0"
+                stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+            </svg>
+        </button>
     </div>
 </div>
-@script
-    <script>
-        document.addEventListener('livewire:initialized', () => {
-            // console.log(Livewire.getByName('admin.products.create.admin-create-product'));
-            // console.log(Livewire.all())
+<script>
+    document.addEventListener('livewire:initialized', () => {
+        Livewire.on('images_received', () => {
+            setTimeout(() => {
+                const swiper = new Swiper('.mySwiper', {
+                    effect: 'fade',
+                    loop: true,
+                    navigation: {
+                        nextEl: ".swiper-button-next",
+                        prevEl: ".swiper-button-prev",
+                    },
+                })
+            }, 200);
         })
-    </script>
-@endscript
+    })
+</script>
