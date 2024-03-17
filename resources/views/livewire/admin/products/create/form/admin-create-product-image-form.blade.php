7af1a398d6d918e16133092e9e8cc6c18c33710e
MeenaMisae <meenahiwatashi@gmail.com>
2024-03-17 12:34:35 -0300

    wip: 🚧 adicionando o metodo no botao que mostra o proximo passo no form

--- resources/views/livewire/admin/products/create/form/admin-create-product-image-form.blade.php
+++ resources/views/livewire/admin/products/create/form/admin-create-product-image-form.blade.php
@@ -74,7 +74,7 @@
             </div>
         </div>
         <div class="flex items-center justify-center">
-            <button class="ml-6 btn btn-outline btn-sm" x-on:click="currentStep++">
+            <button class="ml-6 btn btn-outline btn-sm" x-on:click="currentStep++" wire:click="nextStep">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.0"
                     stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
