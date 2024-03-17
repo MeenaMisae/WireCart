25772c735bb6ea40cd078e324d8a2a98455c7078
MeenaMisae <meenahiwatashi@gmail.com>
2024-03-17 12:32:33 -0300

    wip: 🚧 adicionando evento que envia as urls das imagens para posterior visualizaçao na revisao

--- app/Livewire/Admin/Products/Create/Form/AdminCreateProductImageForm.php
+++ app/Livewire/Admin/Products/Create/Form/AdminCreateProductImageForm.php
@@ -16,6 +16,16 @@ class AdminCreateProductImageForm extends Component
         $this->files = array_values($this->files);
     }
 
+    public function nextStep(): void
+    {
+        $fileUrls = [];
+        foreach ($this->files as $file) :
+            $fileUrls[] = $file->temporaryUrl();
+        endforeach;
+
+        $this->dispatch('product_images', $fileUrls)->to(AdminCreateProductReviewForm::class);
+    }
+
     public function render()
     {
         return view('livewire.admin.products.create.form.admin-create-product-image-form');
