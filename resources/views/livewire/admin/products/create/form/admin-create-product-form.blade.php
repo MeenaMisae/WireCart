5d47d6abbc7fc502160f83166ebfba64b0befd00
meenahiwatashi <meenahiwatashi@gmail.com>
2024-03-20 11:23:18 -0300

    fix: 💄 repaginando ui do formulario de criação do produto

--- resources/views/livewire/admin/products/create/form/admin-create-product-form.blade.php
+++ resources/views/livewire/admin/products/create/form/admin-create-product-form.blade.php
@@ -1,104 +1,112 @@
-<div class="w-3/4 h-full py-4 rounded-lg shadow bg-base-200 mt-7" x-show="currentStep === 1" x-transition.500ms>
-    <div class="grid h-full grid-cols-8 grid-rows-4">
-        <div class="flex items-center justify-center col-start-1 row-start-4">
+<div class="py-4 rounded-lg shadow bg-base-200 mt-7 h-[70vh] w-[70vw]" x-show="currentStep === 1" x-transition.500ms>
+    <div class="grid grid-cols-8 grid-rows-5 h-full w-full gap-6">
+        <div class="col-start-1 row-start-3 flex justify-center items-center">
             <button class="btn btn-outline btn-sm"
                 x-on:click="showProductForm = ! showProductForm; showNavbar = ! showNavbar">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.0"
                     stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18" />
                 </svg>
             </button>
         </div>
-        <div class="flex flex-col items-center max-h-full col-span-2 col-start-2">
-            <label for="" class="label">nome do produto:</label>
-            <input wire:model.live="productName" type="text" class="w-64 input input-bordered">
-            <div class="h-7">
-                @error('productName')
-                    <span class="lowercase text-error">{{ $message }}</span>
-                @enderror
+        <div class="col-start-3 col-span-2">
+            <div class="flex justify-center flex-col items-center w-full">
+                <label for="" class="label">nome do produto:</label>
+                <input type="text" wire:model.live="productName" class="input-bordered input w-64">
+                <div class="w-full flex justify-center mt-1">
+                    @error('productName')
+                        <span class="lowercase text-error">{{ $message }}</span>
+                    @enderror
+                </div>
             </div>
         </div>
-        <div class="flex flex-col items-center max-h-full col-span-2 col-start-4 ml-6">
-            <label for="categoryID" class="label">categoria:</label>
-            <select wire:model.live="categoryID" wire:change="loadSubcategories" name="categoryID" id="categoryID"
-                class="select select-bordered w-52">
-                <option value="0" selected disabled>selecione a categoria</option>
-                @foreach ($categories as $category)
-                    <option value="{{ $category->id }}">{{ $category->name }}</option>
-                @endforeach
-            </select>
-            <div class="h-2">
-                @error('categoryID')
-                    <span class="lowercase text-error">{{ $message }}</span>
-                @enderror
+        <div class="col-span-2 col-start-3 row-start-2 row-span-2 mt-4">
+            <div class="flex flex-col w-64 h-[14rem]">
+                <label for="" class="label w-64 flex justify-center">descrição do produto:</label>
+                <textarea wire:model.live="productDescription" class="h-full w-full resize-none textarea textarea-bordered"></textarea>
+                <span
+                    class="label-text @error('productDescription') text-error @enderror flex justify-end">{{ strlen($productDescription) }}/200</span>
+                <div class="h-2">
+                    @error('productDescription')
+                        <span class="lowercase text-error">{{ $message }}</span>
+                    @enderror
+                </div>
+            </div>
+        </div>
+        <div class="col-start-5 col-span-2">
+            <div class="flex flex-col">
+                <label for="categoryID" class="label w-52 flex justify-center">categoria:</label>
+                <select wire:model.live="categoryID" wire:change="loadSubcategories" name="categoryID" id="categoryID"
+                    class="select select-bordered w-52">
+                    <option value="0" selected disabled>selecione a categoria</option>
+                    @foreach ($categories as $category)
+                        <option value="{{ $category->id }}">{{ $category->name }}</option>
+                    @endforeach
+                </select>
+                <div class="h-2 ml-9 mt-1">
+                    @error('categoryID')
+                        <span class="lowercase text-error">{{ $message }}</span>
+                    @enderror
+                </div>
             </div>
         </div>
         @isset($subcategories)
-            @if (count($subcategories))
-                <div class="flex flex-col items-center col-span-2 col-start-6" wire:transition.duration.150ms>
-                    <label for="" class="label">subcategoria:</label>
+            @php
+                if ($errors->any()):
+                    $this->resetErrorBag();
+                endif;
+            @endphp
+            <div class="col-start-5 row-start-2 mt-4">
+                <div class="flex flex-col">
+                    <label for="" class="label w-52 flex justify-center">subcategoria:</label>
                     <select wire:model.live="subcategoryID" name="subcategoryID" id="subcategoryID"
                         class="select select-bordered w-52">
-                        <option value="0" selected disabled>selecione a subcategoria</option>
+                        <option selected disabled value="0">selecione a subcategoria</option>
                         @foreach ($subcategories as $subcategory)
                             <option value="{{ $subcategory->id }}">{{ $subcategory->name }}</option>
                         @endforeach
                     </select>
-                    <div class="h-2">
+                    <div class="h-auto w-[24rem] mt-1 ml-6">
                         @error('subcategoryID')
                             <span class="lowercase text-error">{{ $message }}</span>
                         @enderror
                     </div>
                 </div>
-            @endif
+            </div>
         @endisset
-        <div class="flex flex-col items-center col-span-2 col-start-2 row-span-2 row-start-2">
-            <div class="flex items-center">
-                <label for="" class="label mr-1.5">descrição do produto:</label>
-                <span
-                    class="badge badge-info badge-sm
-                    @error('productDescription') badge-error @enderror">
-                    {{ strlen($productDescription) }}
-                </span>
-            </div>
-            <textarea wire:model.live="productDescription" type="text"
-                class="w-full h-full resize-none textarea textarea-bordered"></textarea>
-            <div class="h-2">
-                @error('productDescription')
-                    <span class="lowercase text-error">{{ $message }}</span>
-                @enderror
-            </div>
-        </div>
-        <div class="flex flex-col items-center col-span-2 col-start-4 row-start-2 ml-6">
-            <label for="" class="label">preço:</label>
-            <input wire:model.live="productPrice" wire:keyup="calculateDiscount" type="number" step="5"
-                min="0" placeholder="0.00" class="input input-bordered w-52">
-            <div class="h-2">
-                @error('productPrice')
-                    <span class="lowercase text-error">{{ $message }}</span>
-                @enderror
-            </div>
-        </div>
-        <div class="flex flex-col items-center col-span-2 col-start-6 row-start-2">
-            <div class="flex">
+        <div class="col-start-5 row-start-3 mt-5">
+            <div class="flex items-center gap-2">
+                <div>
+                    <label for="" class="label">preço:</label>
+                    <input wire:model.live="productPrice"
+                        @if ($onSale) wire:keyup="calculateDiscount" @endif type="number"
+                        step="5" min="0" placeholder="0.00" class="input input-bordered w-28">
+                    <div class="h-2">
+                        @error('productPrice')
+                            <span class="lowercase text-error">{{ $message }}</span>
+                        @enderror
+                    </div>
+                </div>
                 <div>
                     <label for="" class="label">qtd:</label>
                     <input wire:model.live="productQuantity" type="number" step="1" min="0"
-                        placeholder="0" class="input input-bordered w-36">
-                    <div class="h-2 w-36">
+                        placeholder="0" class="input input-bordered w-28">
+                    <div class="h-2">
                         @error('productQuantity')
                             <span class="lowercase text-error">{{ $message }}</span>
                         @enderror
                     </div>
                 </div>
-                <div class="flex flex-col items-center">
-                    <label for="" class="label">promocional?</label>
-                    <input wire:model.live="onSale" type="checkbox" class="checkbox">
-                </div>
+            </div>
+        </div>
+        <div class="col-start-4 row-start-4">
+            <div class="flex items-center mt-8 ml-12">
+                <label for="" class="label">promocional?</label>
+                <input wire:model.live="onSale" type="checkbox" class="checkbox">
             </div>
         </div>
         @if ($onSale)
-            <div class="w-full col-span-3 col-start-4 row-start-3 mt-8 ml-14" wire:transition.duration.150ms>
+            <div class="w-full col-span-3 col-start-3 row-start-4 ml-14 mt-20" wire:transition.duration.150ms>
                 <div class="flex items-center w-full">
                     <label for="" class="label">
                         desconto:
@@ -135,10 +143,9 @@
                 </div>
             </div>
         @endif
-
-        <div class="flex items-center justify-center col-start-8 row-start-4">
-            <button class="btn btn-outline btn-sm" @if ($errors->any()) disabled @endif
-                x-on:click="currentStep++" wire:click="nextStep">
+        <div class="col-start-8 row-start-3 flex justify-center items-center">
+            <button class="btn btn-outline btn-sm" wire:click="validateStep" id="btnNextStep"
+                @if ($errors->any()) disabled @endif>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.0"
                     stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round"
@@ -148,3 +155,17 @@
         </div>
     </div>
 </div>
+@script
+    <script>
+        document.addEventListener('livewire:initialized', () => {
+            Livewire.on('step_validated', () => {
+                const nextStep = $('button#btnNextStep')
+                setTimeout(() => {
+                    nextStep.attr('wire:click', 'nextStep')
+                    nextStep.attr('x-on:click', 'currentStep++')
+                }, 100);
+                nextStep.click()
+            })
+        })
+    </script>
+@endscript
